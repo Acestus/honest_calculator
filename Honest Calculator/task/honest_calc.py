fde4d3fc8c8f3b7f1be74997cd8071f7bc7b8da4
Acestus <architect@acestus.com>
2022-12-06 00:13:36 -0600

    added stage 1 of 5

--- Honest Calculator/task/honest_calc.py
+++ Honest Calculator/task/honest_calc.py
@@ -1,1 +1,22 @@
-# write your code here
+msg_0 = "Enter an equation"
+msg_1 = "Do you even know what numbers are? Stay focused!"
+msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
+
+print(msg_0)
+
+while True:
+    equation = input()
+    split_equation = equation.split()
+    try:
+        float(split_equation[0])
+        float(split_equation[2])
+        if split_equation[1] not in ["+", "-", "*", "/"]:
+            print(msg_2)
+            print(msg_0)
+            continue
+    except ValueError:
+        print(msg_1)
+        print(msg_0)
+        continue
+    else:
+        break
